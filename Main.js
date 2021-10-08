96f0cf132a67cbe2dff454f5f336327c305b8927
Sean Rowan <sean@columntax.com>
2021-10-07 21:54:33 -0700

    Add sample backend

--- Main.js
+++ Main.js
@@ -1,36 +1,49 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {encode as btoa} from 'base-64';
+import axios from "axios";
 
-const token = "<token>"
+const serverUrl = 'http://localhost:3003/token'
+
 
 // Sandbox URL: "https://app-sandbox.columnapi.com/sdk"
 // Production URL: "https://app.columnapi.com/sdk"
-const build_url = (token, base_url = "https://app-sandbox.columnapi.com/sdk") => {
+const build_url = (token, base_url = "http://localhost:3001/sdk") => {
     let param_string = JSON.stringify({token: token});
     let params = btoa(param_string);
     return `${base_url}?params=${params}`;
 }
 
-export default class Main extends Component {
-    openColumnModule = () => {
-        this.props.navigation.navigate("ColumnModuleView", {
+const Main = (props) => {
+    const [token, setokenToken] = useState('')
+
+    const fetchToken = () => {
+        axios.get(serverUrl)
+            .then((body) => {
+                setokenToken(body.data)
+            })
+    };
+
+    useEffect(() => {
+        fetchToken();
+    }, []);
+
+    const openColumnModule = () => {
+        props.navigation.navigate("ColumnModuleView", {
             url: build_url(token)
         });
     };
 
-    render() {
-        return (
-            <View style={styles.view}>
-                <Text>Click below to open the Column Module:</Text>
-                <StatusBar style="auto"/>
-                <TouchableOpacity onPress={this.openColumnModule} style={styles.touchable}>
-                    <Text style={styles.text}>Open Column</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+    return (
+        <View style={styles.view}>
+            <Text>Click below to open the Column Module:</Text>
+            <StatusBar style="auto"/>
+            <TouchableOpacity onPress={openColumnModule} style={styles.touchable}>
+                <Text style={styles.text}>Open Column</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -50,5 +63,8 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     text: {
-        color: "#fff", }
+        color: "#fff",
+    }
 });
+
+export default Main;
---
+++ server/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+var axios = require('axios');
+const app = express()
+const port = 3003
+const clientId = '<client_id>'
+const clientSecret = '<client_secret>'
+const columnUrl = "https://sandbox.columnapi.com/v1/users"
+// const columnUrl = "http://localhost:3000/v1/users"
+
+app.get('/token', (req, res) => {
+    let apiKey = `${clientId}:${clientSecret}`;
+    let auth = Buffer.from(apiKey).toString('base64')
+
+    const body = JSON.stringify({
+        user_identifier: new Date().getTime(),
+        first_name: "test",
+        last_name: "test",
+        date_of_birth: "1800-01-01",
+        social_security_number: "AAA-GG-SSSS",
+        email: "test@test.com",
+        should_switch_direct_deposit: false,
+        address: {
+            street_1: "test",
+            city: "test",
+            state: "test",
+            zip_code: "test"
+        }
+    })
+
+    const headers = {
+        Authorization: `Basic ${auth}`,
+        "Content-Type": "application/json",
+    };
+
+    axios.post(columnUrl, body, {headers})
+        .then((body) => {
+            res.send(body.data["user_token"])
+        })
+        .catch((error) => {
+            res.send("Bad API Keys or Wrong URL")
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
