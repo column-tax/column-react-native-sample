0ab4170ea87deefb61512aa5ee73abe309518e44
billy1kaplan <billy@columntax.com>
2022-11-03 22:34:39 -0400

    Open iframe links within Webview + External ColumnTax links in Browser (#15)

--- ColumnModuleView.js
+++ ColumnModuleView.js
@@ -27,17 +27,31 @@ export default class ColumnModuleView extends Component {
           }}
           setSupportMultipleWindows={false}
           onShouldStartLoadWithRequest={(request) => {
+            // Always open iframes within the Webview
+            // Otherwise, the iframe may open
+            // within the external browser.
+            if (!request.isTopFrame) return true;
+
+            // Only allow certain domains to take over the webview
+            // This allows us to open certain links externally, like
+            // the IRS website.
             const allowedDomains = [
               "localhost",
               "columnapi.com",
               "env.bz"
             ];
-            const hostname = new URL(request.url).hostname;
+            const url = new URL(request.url);
 
-            if (allowedDomains.find((d) => hostname === d || hostname.endsWith(`.${d}`))) {
+            // Mechanism to open some links on our domain externally
+            const isExternalLink = url.searchParams.get("columntax-external-link") === "true";
+            const hostname = url.hostname;
+
+            if (allowedDomains.find((d) => hostname === d || hostname.endsWith(`.${d}`)) && !isExternalLink) {
               return true;
             } else {
               Linking.openURL(request.url).catch(() => null);
+
+              // Prevent the external link from opening within the webview
               return false;
             }
           }}
